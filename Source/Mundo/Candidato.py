1afd53a9571f080f3d74038669a1065d10513c53
Joan Andrés <andres6936@live.com>
2017-07-22 11:57:12 -0500

    Versión Beta 0.8
    Se completa la documentación de la clase Urna y clase Candidato.

--- Source/Mundo/Candidato.py
+++ Source/Mundo/Candidato.py
@@ -6,6 +6,9 @@
 # @Email: andres6936@live.com
 
 class Candidato:
+    """
+    Candidato de la elección.
+    """
 
     # ---------------------------------
     # Atributos
@@ -18,53 +21,122 @@ class Candidato:
     # ---------------------------------
 
     def __init__(self, nNombre, nApellido, nPartidoPolitico, nEdad):
+        """
+        Inicializa los datos del candidato con los valores que vienen por parámetro.
+        @postcondition: El costo de campaña se inicializó en cero.
+        @param nNombre: Nombre del candidato.
+        @type nNombre: str
+        @param nApellido: Apellido del candidato.
+        @type nApellido: str
+        @param nPartidoPolitico: Partido político del candidato.
+        @type nPartidoPolitico: str
+        @param nEdad: Edad del candidato.
+        @type nEdad: int
+        """
 
-        self.nombre = nNombre
-        self.apellido = nApellido
-        self.partidoPolitico = nPartidoPolitico
-        self.edad = nEdad
-        self.costoCampanha = 0
-        self.votos = 0
+        self.nombre: str = nNombre
+        self.apellido: str = nApellido
+        self.partidoPolitico: str = nPartidoPolitico
+        self.edad: int = nEdad
+        self.costoCampanha: float = 0
+        self.votos: int = 0
 
     # ---------------------------------
     # Métodos
     # ---------------------------------
 
-    def GetNombre(self):
+    def GetNombre(self) -> str:
+        """
+        Devuelve el nombre del candidato.
+        @return: Nombre del candidato.
+        @rtype: str
+        """
         return self.nombre
 
-    def GetApellido(self):
+    def GetApellido(self) -> str:
+        """
+        Devuelve el apellido del candidato.
+        @return: Apellido del candidato.
+        @rtype: str
+        """
         return self.apellido
 
-    def GetPartidoPolitico(self):
+    def GetPartidoPolitico(self) -> str:
+        """
+        Devuelve el partido político del candidato.
+        @return: Partido político del candidato.
+        @rtype: str
+        """
         return self.partidoPolitico
 
-    def GetEdad(self):
+    def GetEdad(self) -> int:
+        """
+        Devuelve la edad del candidato.
+        @return: Edad del candidato.
+        @rtype: int
+        """
         return self.edad
 
-    def GetCostoCampanha(self):
+    def GetCostoCampanha(self) -> float:
+        """
+        Devuelve el costo de campaña del candidato.
+        @return: Costo de campaña del candidato.
+        @rtype: float
+        """
         return self.costoCampanha
 
-    def GetVotos(self):
+    def GetVotos(self) -> int:
+        """
+        Devuelve el número de votos del candidato.
+        @return: Número de votos obtenidos.
+        @rtype: int
+        """
         return self.votos
 
-    def IngresarUnVoto(self):
+    def IngresarUnVoto(self) -> None:
+        """
+        Ingresa un voto al candidato.
+        @postcondition: Se modificó el número de votos aumentándose el existente en 1.
+        """
         self.votos += 1
 
-    def AgregarVotoTelevision(self):
+    def AgregarVotoTelevision(self) -> None:
+        """
+        Adiciona un voto influenciado por la televisión.
+        @postcondition: Se adiciona al costo de la camapaña
+        la suma de $1000 y se incrementa el número de votos en 1.
+        """
         self.costoCampanha += 1000
         self.IngresarUnVoto()
 
-    def AgregarVotoRadio(self):
+    def AgregarVotoRadio(self) -> None:
+        """
+        Adiciona un voto influenciado por la radio.
+        @postcondition: Se adiciona al costo de la campaña la suma de $500 y se
+        incrementa el número de votos en 1.
+        """
         self.costoCampanha += 500
         self.IngresarUnVoto()
 
-    def AgregarVotoInternet(self):
+    def AgregarVotoInternet(self) -> None:
+        """
+        Adiciona un voto influenciado por internet.
+        @postcondition: Se adiciona al costo de la camapaña la suma de $100
+        y se incrementa el número de votos en 1.
+        """
         self.costoCampanha += 100
         self.IngresarUnVoto()
 
-    def ReiniciarConteoVotos(self):
+    def ReiniciarConteoVotos(self) -> None:
+        """
+        Se reinicia el conteo de votos.
+        @postcondition: self.votos = 0
+        """
         self.votos = 0
 
     def ReiniciarCostoCampanha(self):
+        """
+        Se reinicia el costo de camapaña.
+        @postcondition: self.costoCampanha = 0
+        """
         self.costoCampanha = 0
--- Source/Mundo/Urna.py
+++ Source/Mundo/Urna.py
@@ -8,67 +8,140 @@
 from Source.Mundo.Candidato import Candidato
 
 class Urna:
+    """
+    Es la urna de votación con tres candidatos.
+    """
+
+    # ---------------------------------
+    # Atributos
+    # ---------------------------------
+
+
 
     # ---------------------------------
     # Constructor
     # ---------------------------------
 
     def __init__(self):
+        """
+        Inicializa los tres candidatos.
+        @postcondition: Se inicializaron los tres candidatos con los valores por parámetro
+        nombre, apellido, partido político y edad.
+        """
 
         # Inicializa el candidato 1.
         self.candidato1 = Candidato('Felipe', 'Pitti', 'Independiente', 27)
+        """
+        El candidato número 1 de las elecciones.
+        """
 
         # Inicializa el candidato 2.
         self.candidato2 = Candidato('Susanita', 'Chirusi', 'Revolucionario', 26)
+        """
+        El candidato número 2 de las elecciones.
+        """
 
         # Inicializa el candidato 3.
         self.candidato3 = Candidato('Manolito', 'Goreiro', 'Tradicional', 26)
+        """
+        El candidato número 3 de las elecciones.
+        """
 
     # ---------------------------------
     # Métodos
     # ---------------------------------
 
 
-    def GetCandidato1(self):
+    def GetCandidato1(self) -> Candidato:
+        """
+        Devuelve el candidato 1.
+        @return: Candidato 1
+        """
         return self.candidato1
 
-    def GetCandidato2(self):
+    def GetCandidato2(self) -> Candidato:
+        """
+        Devuelve el candidato 2.
+        @return: Candidato 2.
+        """
         return self.candidato2
 
-    def GetCandidato3(self):
+    def GetCandidato3(self) -> Candidato:
+        """
+        Devuelve el candidato 3.
+        @return: Candidato 3.
+        """
         return self.candidato3
 
-    def IngresarVotoTelevisionCandidato1(self):
+    def IngresarVotoTelevisionCandidato1(self) -> None:
+        """
+        Ingresa un voto influenciado por la televisión al candidato 1.
+        """
         self.candidato1.AgregarVotoTelevision()
 
-    def IngresarVotoRadioCandidato1(self):
+    def IngresarVotoRadioCandidato1(self) -> None:
+        """
+        Ingresa un voto influenciado por radio al candidato 1.
+        """
         self.candidato1.AgregarVotoRadio()
 
-    def IngresarVotoInternetCandidato1(self):
+    def IngresarVotoInternetCandidato1(self) -> None:
+        """
+        Ingresa un voto influenciado por internet al candidato 1.
+        """
         self.candidato1.AgregarVotoInternet()
 
-    def IngresarVotoTelevisionCandidato2(self):
+    def IngresarVotoTelevisionCandidato2(self) -> None:
+        """
+        Ingresa un voto influenciado por la televisión al candidato 2.
+        """
         self.candidato2.AgregarVotoTelevision()
 
-    def IngresarVotoRadioCandidato2(self):
+    def IngresarVotoRadioCandidato2(self) -> None:
+        """
+        Ingresa un voto influenciado por radio al candidato 2.
+        """
         self.candidato2.AgregarVotoRadio()
 
-    def IngresarVotoInternetCandidato2(self):
+    def IngresarVotoInternetCandidato2(self) -> None:
+        """
+        Ingresa un voto influenciado por internet al candidato 2.
+        """
         self.candidato2.AgregarVotoInternet()
 
-    def IngresarVotoTelevisionCandidato3(self):
+    def IngresarVotoTelevisionCandidato3(self) -> None:
+        """
+        Ingresa un voto influenciado por la televisión al candidato 3.
+        """
         self.candidato3.AgregarVotoTelevision()
 
-    def IngresarVotoRadioCandidato3(self):
+    def IngresarVotoRadioCandidato3(self) -> None:
+        """
+        Ingresa un voto influenciado por radio al candidato 3.
+        """
         self.candidato3.AgregarVotoRadio()
 
-    def IngresarVotoInternetCandidato3(self):
+    def IngresarVotoInternetCandidato3(self) -> None:
+        """
+        Ingresa un voto influenciado por internet al candidato 3.
+        """
         self.candidato3.AgregarVotoInternet()
 
-    def CalcularTotalVotos(self):
+    def CalcularTotalVotos(self) -> int:
+        """
+        Devuelve el total de votos en la urna.
+        @return: La sumatoria de los votos de los tres candidatos.
+        @rtype: int
+        """
         return self.candidato1.GetVotos() + self.candidato2.GetVotos() + self.candidato3.GetVotos()
 
     def CalcularCostoPromedioCampanha(self) -> float:
+        """
+        Devuelve el costo promedio de camapaña de los candidatos.
+        @return: La razón de la sumatoria de los costos de la campaña de los
+        candidatos y el número total de candidatos.
+        @rtype: float
+        """
 
         total: int = self.candidato1.GetCostoCampanha() + self.candidato2.GetCostoCampanha() + self.candidato3.GetCostoCampanha()
         promedio: float = total / 3
@@ -76,6 +149,11 @@ class Urna:
         return promedio
 
     def CalcularPorcentajeVotosCandidato1(self) -> float:
+        """
+        Devuelve el porcentaje de votos obtenidos por el candidato 1.
+        @return: Porcentaje de votos obtenidos por el candidato 1.
+        @rtype: float
+        """
 
         numVotosCandidato1: int = self.candidato1.GetVotos()
         votosTotales: int = self.CalcularTotalVotos()
@@ -85,6 +163,11 @@ class Urna:
         return porcentaje
 
     def CalcularPorcentajeVotosCandidato2(self) -> float:
+        """
+        Devuelve el porcentaje de votos obtenidos por el candidato 2.
+        @return: Porcentaje de votos obtenidos por el candidato 2.
+        @rtype: float
+        """
 
         numVotosCandidato2: int = self.candidato2.GetVotos()
         votosTotales: int = self.CalcularTotalVotos()
@@ -93,7 +176,12 @@ class Urna:
 
         return porcentaje
 
-    def CalcularPorcentajeVotosCandidato3(self):
+    def CalcularPorcentajeVotosCandidato3(self) -> float:
+        """
+        Devuelve el porcentaje de votos obtenidos por el candidato 3.
+        @return: Porcentaje de votos obtenidos por el candidato 3.
+        @rtype: float
+        """
 
         numVotosCandidato3: int = self.candidato3.GetVotos()
         votosTotales: int = self.CalcularTotalVotos()
@@ -102,7 +190,11 @@ class Urna:
 
         return porcentaje
 
-    def VaciarUrna(self):
+    def VaciarUrna(self) -> None:
+        """
+        Restaura la urna al estado inicial, todos los candidatos qeudan sin votos y
+        costo de campaña en 0.
+        """
 
         # Reiniciar candidato 1.
         self.candidato1.ReiniciarConteoVotos()
@@ -121,7 +213,15 @@ class Urna:
     # ---------------------------------
 
     def Metodo1(self) -> str:
+        """
+        Método para la extensión 1.
+        @return: Respuesta 1.
+        """
         return 'Respuesta 1'
 
     def Metodo2(self) -> str:
+        """
+        Método para la extensión 2.
+        @return: Respuesta 2.
+        """
         return 'Respuesta 2'
