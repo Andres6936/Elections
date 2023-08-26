74b074e0948d79b427c8de02a4d4ffc16bc82263
Andres6936 <andres6936@gmail.com>
2023-08-26 12:08:52 -0500

    Added Types

--- Services/Models/Personal/Personal.py
+++ Services/Models/Personal/Personal.py
@@ -1,5 +1,18 @@
+from peewee import TextField, IntegerField
+
 from Services.Models.Personal.PersonalBase import PersonalBase
 
 
 class Personal(PersonalBase):
-    pass
+    TypeDocument = TextField()
+    NumberDocument = IntegerField()
+    Names = TextField()
+    BornPlace = TextField()
+    Country = TextField()
+    Profession = TextField()
+    Experience = TextField()
+    Role = TextField()
+    Dependency = TextField()
+    Email = TextField()
+    Phone = IntegerField()
+    Salary = TextField()
--- Services/Models/Personal/PersonalCOVI.py
+++ Services/Models/Personal/PersonalCOVI.py
@@ -1,5 +1,18 @@
+from peewee import DoubleField, TextField, IntegerField
+
 from Services.Models.Personal.PersonalBase import PersonalBase
 
 
 class PersonalCOVI(PersonalBase):
-    pass
+    Nit = DoubleField()
+    Business = TextField()
+    TypeBusiness = IntegerField()
+    NumberDocument = TextField()
+    Names = TextField()
+    Profession = TextField()
+    TypeVehicle = TextField()
+    NumberVehicle = TextField()
+    Phone = TextField()
+    Address = TextField()
+    Age = TextField()
+    Eps = TextField()
