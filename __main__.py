bb6c6aeff4702eebcd48b6c4d85c847964f1a7df
Andres6936 <andres6936@gmail.com>
2023-08-26 11:13:00 -0500

    Added Pipenv and DPH Awareness

--- __main__.py
+++ __main__.py
@@ -5,9 +5,15 @@
 # @Email: andres6936@gmail.com
 # @Email: andres6936@live.com
 
+import ctypes
+
+try:
+    ctypes.windll.shcore.SetProcessDpiAwareness(True)
+except:
+    pass
+
 from App import AplicationManager
 
 if __name__ == '__main__':
-
     app = AplicationManager()
     app.MainLoop()
