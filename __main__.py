bd116608c48f0a6be24ee03b1bb23761a6aee75c
Andres6936 <andres6936@gmail.com>
2023-10-14 20:51:20 -0500

    Removed Migration Files

--- __main__.py
+++
@@ -1,7 +0,0 @@
-from Services.Migrations.AddUUID import AddUUIDSenatorExpense
-
-if __name__ == '__main__':
-    # AddUUID()
-    # AddUUIDCandidates()
-    # ADDUUIDSenator()
-    AddUUIDSenatorExpense()
