d7b0d8ddc40332f58ccc43b0ab9b071cce33a21c
Andres6936 <andres6936@gmail.com>
2023-11-23 08:18:16 -0500

    Added React Navigator

--- app/App.tsx
+++ app/App.tsx
@@ -1,106 +1,16 @@
-import React, {useEffect, useState} from 'react';
-import {Button, SafeAreaView, ScrollView, StatusBar, Text, useColorScheme, View,} from 'react-native-windows';
-import {CandidateService} from "./src/services/CandidateService";
-import {TypeCandidates} from "./src/types/TypeCandidates";
+import React from 'react';
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {CandidateScreen} from "./src/screens/CandidateScreen";
 
+const Stack = createNativeStackNavigator();
 
-function App(): JSX.Element {
-    const isDarkMode = useColorScheme() === 'dark';
-    const [isPaginationEnd, setIsPaginationEnd] = useState<boolean>(false)
-    const [isError, setIsError] = useState([false, "Not Error"])
-    const [currentPage, setCurrentPage] = useState<number>(0)
-    const [pageSize, setPageSize] = useState<number>(60);
-    const [candidates, setCandidates] = useState<TypeCandidates[]>([])
-
-    useEffect(() => {
-        (async () => {
-            try {
-                if (isPaginationEnd) return;
-
-                const candidateService = new CandidateService();
-                const candidates = await candidateService.getAllCandidates({
-                    skip: currentPage,
-                    limit: pageSize,
-                });
-                // If the results of endpoint are lesser that the required for the pageSize, reach the page end
-                if (candidates.length < pageSize) setIsPaginationEnd(true)
-
-                setCandidates(candidates)
-            } catch (exception) {
-                setIsError([true, "Promise Rejected"])
-            }
-        })()
-    }, [currentPage, pageSize, isPaginationEnd]);
-
-    const backgroundStyle = "text-black bg-neutral-300 dark:bg-slate-900 dark:text-white"
-
-    const previousPage = () => {
-        if (currentPage >= 1) {
-            setCurrentPage(currentPage - 1)
-        }
-    }
-
-    const nextPage = () => {
-        if (isPaginationEnd) return
-
-        setCurrentPage(currentPage + 1)
-    }
-
+export default function App() {
     return (
-        <SafeAreaView className={"flex-1 flex-row bg-white"}>
-            <StatusBar
-                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-            />
-            <View className="flex-[2] gap-4 p-2">
-                <View className="border-b pb-2">
-                    <Text className="font-semibold">Candidates</Text>
-                </View>
-                <View className="border-b pb-2">
-                    <Text className="font-semibold">Elections National</Text>
-                </View>
-                <View className="border-b pb-2">
-                    <Text className="font-semibold">Elections Native</Text>
-                </View>
-                <View className="border-b pb-2">
-                    <Text className="font-semibold">Income Base</Text>
-                </View>
-                <View className="border-b pb-2">
-                    <Text className="font-semibold">Items</Text>
-                </View>
-                <View className="border-b pb-2">
-                    <Text className="font-semibold">Senator</Text>
-                </View>
-                <View className="border-b pb-2">
-                    <Text className="font-semibold">Senator Expenses</Text>
-                </View>
-            </View>
-            <View className="flex-[9] border-l">
-                <ScrollView
-                    contentInsetAdjustmentBehavior="automatic"
-                    className={backgroundStyle}>
-                    <View className="bg-white dark:bg-black p-2 gap-2">
-                        {candidates.map(candidate => (
-                            <View key={candidate.Serial} className="flex-1 border border-red-200 rounded p-2">
-                                <Text className="font-bold text-md">Candidate: {candidate.Candidate}</Text>
-                                <Text>Political Party: {candidate.PoliticalParty}</Text>
-                                <View className="flex-row mt-2">
-                                    <Button className="w-24 bg-sky-500" title="Edit"></Button>
-                                    <Button className="w-24 bg-red-500" title="Delete"></Button>
-                                </View>
-                            </View>
-                        ))}
-                    </View>
-                </ScrollView>
-                <View className="flex-row p-2 bg-white border-t border-indigo-500 items-center">
-                    <Text className="text-black mr-3">Show: {pageSize} registers of {currentPage + 1} page</Text>
-                    <Button onPress={previousPage} disabled={currentPage === 0} className="bg-blue-500 text-black"
-                            title="Prev">Prev</Button>
-                    <Button onPress={nextPage} disabled={isPaginationEnd} className="bg-blue-500 text-black"
-                            title="Next">Next</Button>
-                </View>
-            </View>
-        </SafeAreaView>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName={"Candidate"}>
+                <Stack.Screen name={"Candidate"} component={CandidateScreen}/>
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
-
-export default App;
