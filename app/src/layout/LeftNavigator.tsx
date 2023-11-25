e7d9cbc40c8739b37c9f25d494a6306c427483e8
Andres6936 <andres6936@gmail.com>
2023-11-25 17:46:58 -0500

    Change of Style

--- app/src/layout/LeftNavigator.tsx
+++ app/src/layout/LeftNavigator.tsx
@@ -6,27 +6,30 @@ export function LeftNavigator() {
     const navigator = useNavigation();
 
     return (
-        <View className="flex-[2] gap-4 p-2">
-            <Pressable onPress={() => navigator.navigate("CandidateScreen")} className="border-b pb-2">
-                <Text className="font-semibold">Candidates</Text>
+        <View className="bg-blue-600 text-white flex-[2] gap-1 p-2">
+            <Pressable onPress={() => navigator.navigate("CandidateScreen")} className="p-2 rounded hover:bg-sky-400">
+                <Text className="text-white font-semibold">Candidates</Text>
             </Pressable>
-            <Pressable onPress={() => navigator.navigate("ElectionsNationalScreen")} className="border-b pb-2">
-                <Text className="font-semibold">Elections National</Text>
+            <Pressable onPress={() => navigator.navigate("ElectionsNationalScreen")}
+                       className="p-2 rounded hover:bg-sky-400">
+                <Text className="text-white font-semibold">Elections National</Text>
             </Pressable>
-            <Pressable onPress={() => navigator.navigate("ElectionsNativeScreen")} className="border-b pb-2">
-                <Text className="font-semibold">Elections Native</Text>
+            <Pressable onPress={() => navigator.navigate("ElectionsNativeScreen")}
+                       className="p-2 rounded hover:bg-sky-400">
+                <Text className="text-white font-semibold">Elections Native</Text>
             </Pressable>
-            <Pressable onPress={() => navigator.navigate("IncomeBaseScreen")} className="border-b pb-2">
-                <Text className="font-semibold">Income Base</Text>
+            <Pressable onPress={() => navigator.navigate("IncomeBaseScreen")} className="p-2 rounded hover:bg-sky-400">
+                <Text className="text-white font-semibold">Income Base</Text>
             </Pressable>
-            <Pressable onPress={() => navigator.navigate("ItemsScreen")} className="border-b pb-2">
-                <Text className="font-semibold">Items</Text>
+            <Pressable onPress={() => navigator.navigate("ItemsScreen")} className="p-2 rounded hover:bg-sky-400">
+                <Text className="text-white font-semibold">Items</Text>
             </Pressable>
-            <Pressable onPress={() => navigator.navigate("SenatorScreen")} className="border-b pb-2">
-                <Text className="font-semibold">Senator</Text>
+            <Pressable onPress={() => navigator.navigate("SenatorScreen")} className="p-2 rounded hover:bg-sky-400">
+                <Text className="text-white font-semibold">Senator</Text>
             </Pressable>
-            <Pressable onPress={() => navigator.navigate("SenatorExpenseScreen")} className="border-b pb-2">
-                <Text className="font-semibold">Senator Expenses</Text>
+            <Pressable onPress={() => navigator.navigate("SenatorExpenseScreen")}
+                       className="p-2 rounded hover:bg-sky-400">
+                <Text className="text-white font-semibold">Senator Expenses</Text>
             </Pressable>
         </View>
     )
